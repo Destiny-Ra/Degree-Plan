595166487d772b6dd4b8e94008cac6b0199cf51e
mrlovyou <nightprofile.love@gmail.com>
2025-04-10 19:25:48 -0500

    added students and student_courses tables in database so the students email could connect to unique course set.

--- Degree Plan/database/specialization_additions.sql
+++ Degree Plan/database/specialization_additions.sql
@@ -5,14 +5,11 @@ INSERT INTO courses (course_code, title, description, credits, course_type) VALU
 ('COSC 4470', 'Applied Network Security', 'Discusses threats related to the CIA triad, including eavesdropping, man-in-the-middle attacks, and denial-of-service.', 4, 'Comp Sci General'),
 ('COSC 4395', 'Research in Cyber Security', 'Provides research opportunities in a selected field of cyber security.', 3, 'Comp Sci General'),
 
-
-
 ('COSC 4415', 'Database Systems', 'Focuses on relational data models, ER schema design, database design, and functional dependencies in commercial systems.', 4, 'Comp Sci General'),
 ('COSC 4455', 'Multimedia and Web Development', 'Covers web and multimedia development, including HTML, XML, graphics, animation, audio, and video processing tools.', 4, 'Comp Sci General'),
 ('COSC 4460', 'Software Engineering', 'Introduces fundamental concepts in software systems development, visual modeling, life cycles, and CASE tools for web-based systems.', 4, 'Comp Sci General'),
 ('COSC 4485', 'Mobile Application Development', 'Teaches mobile application development for smartphones and tablets, focusing on mobile OS features and client-server applications.', 4, 'Comp Sci General'),
-('COSC 4395', 'Research in Software Development', 'Offers research opportunities in a selected field of software development.', 3, 'Comp Sci General')
-
+('COSC 4395', 'Research in Software Development', 'Offers research opportunities in a selected field of software development.', 3, 'Comp Sci General'),
 
 ('ARTS 2348', 'Digital Art', 'Introduces current computer design programs and software for digital art creation.', 3, 'Comp Sci General'),
 ('ARTS 2358', 'Graphic Art: Typography', 'Explores basic vocabulary, theories, principles, and methods related to typography.', 3, 'Comp Sci General'),
